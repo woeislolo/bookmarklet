c89d8babb3507ac54eacd414bf29f0c0b615f0fb
woeislolo <falloutgirl7@mail.ru>
2025-02-27 16:09:12 +0500

    Add photo preview in admin panel for images and accounts

--- bookmarklet/account/admin.py
+++ bookmarklet/account/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Profile
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'date_of_birth', 'photo']
+    list_display = ['user', 'date_of_birth', 'photo', 'get_preview_photo',]
     raw_id_fields = ['user']
+
+    def get_preview_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.url}' width=60>")
+        return None
+
+    get_preview_photo.short_description = 'Превью фото'
--- bookmarklet/images/admin.py
+++ bookmarklet/images/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Image
 
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'slug', 'image', 'created']
+    list_display = ['id', 'title', 'slug', 'total_likes', 'get_preview_photo', 'image', 'created']
+    list_display_links = ['id', 'title', 'slug', 'image']
     list_filter = ['created']
+
+    def get_preview_photo(self, object):
+        if object.image:
+            return mark_safe(f"<img src='{object.image.url}' width=60>")
+        return None
+
+    get_preview_photo.short_description = 'Превью фото'
+    
