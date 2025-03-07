8f4a6d1028e0a5305bace6a542daadf18c8ebf27
woeislolo <falloutgirl7@mail.ru>
2025-03-07 13:34:38 +0500

    feat: add logging for tasks

--- bookmarklet/account/tasks.py
+++ bookmarklet/account/tasks.py
@@ -1,27 +1,39 @@
 from django.core.mail import send_mail
 
 from celery import shared_task
+from celery.utils.log import get_task_logger
 
 from .models import User
 
 
+logger = get_task_logger(__name__)
+
+
 @shared_task
 def task_send_email(user_pk, subject):
-    user = User.objects.get(pk=user_pk)
-    email = user.email
-    name = user.first_name
-    if subject == "Profile's update":
-        message = f"""Кто-то изменил информацию Вашего профиля.\n
-        Если это были не вы, рекомендуется зайти в свой аккаунт и изменить пароль.\n
-        Если это были вы, проигнорируйте это письмо."""
-    else:
-        message = 'Регистрация прошла успешно.'
+    logger.info("Running task...")
+    try:
+        user = User.objects.get(pk=user_pk)
+        email = user.email
+        name = user.first_name
+        if subject == "Profile's update":
+            message = f"""Кто-то изменил информацию Вашего профиля.\n
+            Если это были не вы, рекомендуется зайти в свой аккаунт и изменить пароль.\n
+            Если это были вы, проигнорируйте это письмо."""
+        else:
+            message = 'Регистрация прошла успешно.'
 
-    print(name, email, message)
-    send_mail(
-        subject=f"{subject} of {name}",
-        message=message,
-        from_email="support@example.com",
-        recipient_list=[email],
-        fail_silently=False,
-    )
+        result = send_mail(
+            subject=f"{subject} of {name}",
+            message=message,
+            from_email="support@example.com",
+            recipient_list=[email],
+            fail_silently=False,
+        )
+        if result == 0:
+            logger.error(f"Email failed to send")
+        else:
+            logger.info(f"Email sent successfully")
+    except:
+        logger.error("Could not fetch the result")
+        # raise SystemExit(1)
