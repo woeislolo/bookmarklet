a9288d0b101968995936555d48a4cb7f1e758db5
woeislolo <falloutgirl7@mail.ru>
2025-03-13 13:38:37 +0500

    feat: add Google athentication

--- bookmarklet/bookmarklet/settings.py
+++ bookmarklet/bookmarklet/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = config['project_secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
@@ -46,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'django.contrib.postgres',
-    # 'social_django',
+    'social_django',
     'django_extensions',
     'debug_toolbar',
 
@@ -107,25 +108,25 @@ DATABASES = {
 # auth
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    # 'men.authentication.EmailAuthBackend',
-    # 'social_core.backends.google.GoogleOAuth2',
+    # 'account.authentication.EmailAuthBackend',
+    'social_core.backends.google.GoogleOAuth2',
     ]
 
-# SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config['GOOGLE_OAUTH2_KEY']
-# SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config['GOOGLE_OAUTH2_SECRET']
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config['GOOGLE_OAUTH2_KEY']
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config['GOOGLE_OAUTH2_SECRET']
 
-# SOCIAL_AUTH_PIPELINE = [
-#     'social_core.pipeline.social_auth.social_details',
-#     'social_core.pipeline.social_auth.social_uid',
-#     'social_core.pipeline.social_auth.auth_allowed',
-#     'social_core.pipeline.social_auth.social_user',
-#     'social_core.pipeline.user.get_username',
-#     'social_core.pipeline.user.create_user',
-#     'men.authentication.create_profile',
-#     'social_core.pipeline.social_auth.associate_user',
-#     'social_core.pipeline.social_auth.load_extra_data',
-#     'social_core.pipeline.user.user_details',
-#     ]
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    ]
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
