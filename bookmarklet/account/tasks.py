0b1e96ab75353916dd0dc43e506a43d42e9dc3c3
woeislolo <falloutgirl7@mail.ru>
2025-03-14 11:21:05 +0500

    feat: add periodic task for birthday email

--- bookmarklet/account/tasks.py
+++ bookmarklet/account/tasks.py
@@ -1,9 +1,11 @@
 from django.core.mail import send_mail
+from django.utils import timezone
 
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
-from .models import User
+from account.models import *
+from images.models import *
 
 
 logger = get_task_logger(__name__)
@@ -34,6 +36,33 @@ def task_send_email(user_pk, subject):
             logger.error(f"Email failed to send")
         else:
             logger.info(f"Email sent successfully")
+    except:
+        logger.error("Could not fetch the result")
+        # raise SystemExit(1)
+
+
+@shared_task
+def task_send_birthday_email():
+    logger.info("Running task...")
+    try:
+        today = timezone.now().date()
+        birthday_profile_ids = Profile.objects \
+            .filter(date_of_birth__day=today.day, date_of_birth__month=today.month) \
+            .values_list('user_id', flat=True)
+        birthday_emails = User.objects.filter(id__in=birthday_profile_ids) \
+            .values_list('username', 'email')
+        for name, email in birthday_emails:
+            result = send_mail(
+                subject=f"Birthday wishes for {name}",
+                message='Happy birthday!',
+                from_email="support@example.com",
+                recipient_list=[email],
+                fail_silently=False,
+            )
+            if result == 0:
+                logger.error(f"Email failed to send for email {email}")
+            else:
+                logger.info(f"Email sent successfully for email {email}")
     except:
         logger.error("Could not fetch the result")
         # raise SystemExit(1)
